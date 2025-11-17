b760e282502398d44bb924a72545fe7e825e793c
Sam <109049343+mcalendar@users.noreply.github.com>
2025-11-17 10:11:49 -0500

    nov 17th save

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2418748f0be8ca1b3b13bd8c13137f9c",
+const RESOURCES = {"flutter_bootstrap.js": "bbaca302732da7929c34134007e30803",
 "version.json": "2f450ba10bf8d31a03d4c861314f5d6d",
 "index.html": "838512745437b50d829b0876c52f67b7",
 "/": "838512745437b50d829b0876c52f67b7",
-"main.dart.js": "30641192569af5376f1d698d8901271c",
+"main.dart.js": "84c4db165d5825acfa9ae609a4d20abe",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "2418748f0be8ca1b3b13bd8c13137f9c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/fonts/MaterialIcons-Regular.otf": "c0ad29d56cfe3890223c02da3c6e0448",
+"assets/fonts/MaterialIcons-Regular.otf": "71b459d8ccc4e2d805c537f7aa031eaf",
 "canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "canvaskit/skwasm_heavy.js": "413f5b2b2d9345f37de148e2544f584f",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
